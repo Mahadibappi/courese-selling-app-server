ce0a433253245727e406d6f586a023b04078e523
Mahadi Hasan <69779118+Mahadibappi@users.noreply.github.com>
2022-10-26 14:13:52 +0600

    data created

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 5000;
+const cors = require("cors");
+app.use(cors());
+
+const categories = require("./data/Category.json");
+const courses = require("./data/Course.json");
+
+app.get("/", (req, res) => {
+  res.send("Education server running");
+});
+
+app.get("/single-category", (req, res) => {
+  res.send(categories);
+});
+
+app.get("/courses", (req, res) => {
+  res.send(courses);
+});
+
+app.listen(port, () => {
+  console.log("server running", port);
+});
